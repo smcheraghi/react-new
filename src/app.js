5d29a3e03d79d7e56a2733793af1aa51f32c3be8
seyed mohammad cheraghi <35502397+smcheraghi@users.noreply.github.com>
2021-01-30 01:16:10 -0500

    init project

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// JSX -> JavaScript XML
+var template = <p>This is a test, it is done</p>                             // JSX -> javascript XML --> provide by react
+// var template = React.createElement("p", null, "This is a test");    // changing by babel --> babel is a compiler, has no functionality --> npm i -g babel-cli --> npm init --> npm add babel-preset-react babel-preset-env --> // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --> --out-file=public/scripts/app.js --presets="env,react" --watch
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
+
+
+
